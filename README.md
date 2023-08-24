7fad5e1ba72fc354d96b4d8e9c939e8ca78bb08c
Jacqueline-24 <jacquiguesa99@gmail.com>
2023-08-23 19:10:52 -0500

    Actividad 01

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 -Correo: jacquiguesa99@gmail.com
 
 
+## Actividad 1
+- p01-hola-mundo
+- p02-area-circulo
+- p03-area-triangulo
+- p04-paga-trabajador
---
+++ p02-area-circulo.py
@@ -0,0 +1,13 @@
+# Calcula el area de un circulo dado el radio
+
+import math #importamos la libreria de funciones y constantes matematicas
+
+print("Calculando el area de un circulo/n")
+
+radio= float(input("Dame el radio?"))
+
+area= 3.1416 * radio * radio 
+area= math.pi * math.pow(radio,2)
+
+print(f"Para un circulode radio {radio}el area resultante es {area:.2f}")
+
